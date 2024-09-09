93831f308260018654ee401260463ede117e261d
parthsolanke <parthsolanke2002@gmail.com>
2024-09-09 12:22:58 +0530

    chore: add authMiddleware for files endpoint

--- backend/config/rootRouter.js
+++ backend/config/rootRouter.js
@@ -1,10 +1,11 @@
+// config/rootRouter.js
 const { Router } = require('express')
 const authRoutes = require('../auth/authRoutes');
 const fileRoutes = require('../file/fileRoutes');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = Router();
 
-router.use('auth', authRoutes);
-router.use('files', authMiddleware, fileRoutes);
+router.use('/auth', authRoutes);
+router.use('/files', authMiddleware, fileRoutes);
 
 module.exports = router;
