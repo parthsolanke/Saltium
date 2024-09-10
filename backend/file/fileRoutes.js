c9c14ac7487865cab8870983720ff10cff876381
parthsolanke <parthsolanke2002@gmail.com>
2024-09-10 16:38:18 +0530

    feat: add middlewares to /upload endpoint

--- backend/file/fileRoutes.js
+++ backend/file/fileRoutes.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const fileController = require('./fileController');
-const authMiddleware = require('../middleware/authMiddleware');
+const { uploadMulter, scanForMalware } = require('../middleware/uploadMiddleware')
 
-router.post('/upload', fileController.uploadFile);
+router.post('/upload', uploadMulter.array('files', 10), scanForMalware, fileController.uploadFiles);
 // router.post('/generate-link/:fileId', fileController.generateDownloadLink);
+
 router.get('/download/:fileId', fileController.downloadFile);
 // router.get('/list', fileController.listFiles);
 // router.delete('/:fileId', fileController.deleteFile);
