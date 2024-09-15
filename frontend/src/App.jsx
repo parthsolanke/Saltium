a1f9be47cb395369c3bec92b84d7621cc17144d6
parthsolanke <parthsolanke2002@gmail.com>
2024-09-15 14:29:24 +0530

    chore: add route; refactor and update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '@/pages/LoginPage'
 import RegisterPage from '@/pages/RegisterPage';
+import UploadPage from './pages/FileUploadPage';
+import DownloadPage from './pages/FileDownloadPage';
+import GenerateLinkPage from './pages/GenerateLinkPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<RegisterPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/download" element={<DownloadPage />} />
+          <Route path="/generate-link" element={<GenerateLinkPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </>
